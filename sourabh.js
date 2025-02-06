7c53886bbabc8b84bb5a67e86ac87c46f239411d
sharma816918 <sharmasourabh25006@gmail.com>
2025-02-06 23:05:27 +0530

    Add files via upload

---
+++ sourabh.js
@@ -0,0 +1,7 @@
+// Get the pledge button
+var pledgeButton = document.getElementById("pledge-button");
+
+// Add an event listener to the pledge button
+pledgeButton.addEventListener("click", function() {
+	alert("Thank you for taking the pledge to reduce your plastic use!");
+});
